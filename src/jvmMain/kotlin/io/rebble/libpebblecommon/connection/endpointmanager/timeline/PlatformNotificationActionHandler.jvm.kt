c12704e2e2cd3b88007bd35302b48d65e28f9ac5
crc-32 <me@crc32.dev>
2025-04-11 17:02:50 +0100

    stub PlatformNotificationActionHandler for jvm

---
+++ src/jvmMain/kotlin/io/rebble/libpebblecommon/connection/endpointmanager/timeline/PlatformNotificationActionHandler.jvm.kt
@@ -0,0 +1,16 @@
+package io.rebble.libpebblecommon.connection.endpointmanager.timeline
+
+import io.rebble.libpebblecommon.connection.AppContext
+import io.rebble.libpebblecommon.packets.blobdb.TimelineItem
+import io.rebble.libpebblecommon.services.blobdb.TimelineActionResult
+import kotlin.uuid.Uuid
+
+actual class PlatformNotificationActionHandler actual constructor(appContext: AppContext) {
+    actual suspend operator fun invoke(
+        itemId: Uuid,
+        action: TimelineItem.Action,
+        attributes: List<TimelineItem.Attribute>
+    ): TimelineActionResult {
+        TODO("Not yet implemented")
+    }
+}
