450e2eaf41d2e21204ddd38a7cab9188705745ce
Steve Penna <steve@penna.org.uk>
2025-04-23 12:20:53 -0700

    iOS pbw temp dir

--- src/iosMain/kotlin/io/rebble/libpebblecommon/connection/WatchManager.ios.kt
+++ src/iosMain/kotlin/io/rebble/libpebblecommon/connection/WatchManager.ios.kt
@@ -1,7 +1,14 @@
 package io.rebble.libpebblecommon.connection
 
 import kotlinx.io.files.Path
+import platform.Foundation.NSCachesDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSURL
+import platform.Foundation.NSUserDomainMask
 
 actual fun getTempAppPath(appContext: AppContext): Path {
-    TODO("Not yet implemented")
+    val fm = NSFileManager.defaultManager
+    val nsUrl = fm.URLsForDirectory(NSCachesDirectory, NSUserDomainMask).first()!! as NSURL
+    val path = Path(nsUrl.path!!, "temp.pbw")
+    return path
 }
