d1313f462d65ae7f746a361eb487b3bb0976d7e6
Steve Penna <steve@penna.org.uk>
2025-07-25 14:01:30 -0700

    Use fake FWUP state

--- libpebble3/src/commonMain/kotlin/io/rebble/libpebblecommon/connection/FakeLibPebble.kt
+++ libpebble3/src/commonMain/kotlin/io/rebble/libpebblecommon/connection/FakeLibPebble.kt
@@ -218,7 +218,7 @@ fun fakeWatch(): PebbleDevice {
         FakeConnectedDevice(
             transport = fakeTransport,
             firmwareUpdateAvailable = null,
-            firmwareUpdateState = FirmwareUpdater.FirmwareUpdateStatus.NotInProgress.Idle,
+            firmwareUpdateState = fwupState,
         )
     } else {
         object : DiscoveredPebbleDevice {
