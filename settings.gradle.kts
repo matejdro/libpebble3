e53617c4db734e66006d7448a716a7209e4ea8b8
Matej Drobnič <matej@matejdro.com>
2025-11-02 15:45:35 +0100

    Include PebbleKit2 library

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,3 +12,12 @@ rootProject.name = "libpebbleroot"
 include(":libpebble3")
 include(":blobdbgen")
 include(":blobannotations")
+
+// Include PebbleKit library until
+// it is stable enough to make a release
+includeBuild("PebbleKitAndroid2") {
+    dependencySubstitution {
+        substitute(module("io.rebble.pebblekit2:server"))
+            .using(project(":server"))
+    }
+}
