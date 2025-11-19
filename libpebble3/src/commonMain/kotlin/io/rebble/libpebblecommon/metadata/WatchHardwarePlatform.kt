7a51316824533ad192a89a2f2d4b5edf17bd28f8
Steve Penna <steve@penna.org.uk>
2025-11-19 13:52:41 -0800

    Move isCoreDevice to app

--- libpebble3/src/commonMain/kotlin/io/rebble/libpebblecommon/metadata/WatchHardwarePlatform.kt
+++ libpebble3/src/commonMain/kotlin/io/rebble/libpebblecommon/metadata/WatchHardwarePlatform.kt
@@ -1,12 +1,6 @@
 package io.rebble.libpebblecommon.metadata
 
 import co.touchlab.kermit.Logger
-import io.rebble.libpebblecommon.metadata.WatchHardwarePlatform.CORE_ASTERIX
-import io.rebble.libpebblecommon.metadata.WatchHardwarePlatform.CORE_OBELIX_BIGBOARD
-import io.rebble.libpebblecommon.metadata.WatchHardwarePlatform.CORE_OBELIX_BIGBOARD_2
-import io.rebble.libpebblecommon.metadata.WatchHardwarePlatform.CORE_OBELIX_DVT
-import io.rebble.libpebblecommon.metadata.WatchHardwarePlatform.CORE_OBELIX_EVT
-import io.rebble.libpebblecommon.metadata.WatchHardwarePlatform.CORE_OBELIX_PVT
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.Serializer
 import kotlinx.serialization.descriptors.PrimitiveKind
@@ -62,11 +56,6 @@ enum class WatchHardwarePlatform(val protocolNumber: UByte, val watchType: Watch
             }
         }
     }
-}
-
-fun WatchHardwarePlatform.isCoreDevice(): Boolean = when (this) {
-    CORE_ASTERIX, CORE_OBELIX_EVT, CORE_OBELIX_DVT, CORE_OBELIX_PVT, CORE_OBELIX_BIGBOARD, CORE_OBELIX_BIGBOARD_2 -> true
-    else -> false
 }
 
 @Serializer(WatchHardwarePlatform::class)
