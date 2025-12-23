987375145a754aed612cfe447128f01f40ba3caf
Steve Penna <steve@penna.org.uk>
2025-12-23 16:37:46 +0000

    vibrationPattern can be null

--- libpebble3/src/androidMain/kotlin/io/rebble/libpebblecommon/notification/LibPebbleNotificationListener.kt
+++ libpebble3/src/androidMain/kotlin/io/rebble/libpebblecommon/notification/LibPebbleNotificationListener.kt
@@ -127,7 +127,9 @@ class LibPebbleNotificationListener : NotificationListenerService(), LibPebbleKo
                     id = channel.id,
                     name = channel.name.toString(),
                     muteState = MuteState.Never,
-                    vibrationPattern = channel.getVibrationPattern().map { it.toUInt() }.let { listOf(0u) + it }
+                    vibrationPattern = channel.vibrationPattern?.map { it.toUInt() }?.let {
+                        listOf(0u) + it
+                    }
                 )
                 val group = groups[channel.group]
                 if (group == null) {
