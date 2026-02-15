74b3015b45b65533ed8365033dacc7f31f8e9eb8
Matej Drobnič <matej@matejdro.com>
2026-02-15 23:30:39 +0100

    Add Timeline system app (#112)
    * Add Timeline system app
    
    * Add Timeline app icon
    
    * Move timeline app to the bottom by default

--- libpebble3/src/commonMain/kotlin/io/rebble/libpebblecommon/locker/SystemApps.kt
+++ libpebble3/src/commonMain/kotlin/io/rebble/libpebblecommon/locker/SystemApps.kt
@@ -20,6 +20,7 @@ enum class SystemApps(
     Health(SystemAppIDs.HEALTH_APP_UUID, "Health", AppType.Watchapp, ALL_EXCEPT_APLITE, defaultOrder = -5),
     Workout(SystemAppIDs.WORKOUT_APP_UUID, "Workout", AppType.Watchapp, ALL_EXCEPT_APLITE, defaultOrder = -4),
     Watchfaces(SystemAppIDs.WATCHFACES_APP_UUID, "Watchfaces", AppType.Watchapp, WatchType.entries, defaultOrder = -3),
+    Timeline(SystemAppIDs.TIMELINE_MENU_ENTRY_UUID, "Timeline", AppType.Watchapp, WatchType.entries, defaultOrder = 100),
     // Faces
     Tictoc(SystemAppIDs.TICTOC_APP_UUID, "Tictoc", AppType.Watchface, WatchType.entries, defaultOrder = -2),
     Kickstart(SystemAppIDs.KICKSTART_APP_UUID, "Kickstart", AppType.Watchface, ALL_EXCEPT_APLITE, defaultOrder = -1),
