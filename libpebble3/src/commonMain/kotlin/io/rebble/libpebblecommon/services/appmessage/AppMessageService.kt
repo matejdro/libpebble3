c3662012e9dbbd83684e77d3aa41ef5b6a58b988
Matej Drobnič <matej@matejdro.com>
2026-04-01 00:56:43 +0200

    Fix sendAppMessage erroring out on timeout (#147)
    * Fix sendAppMessage erroring out on timeout
    
    * Remove unused imports

--- libpebble3/src/commonMain/kotlin/io/rebble/libpebblecommon/services/appmessage/AppMessageService.kt
+++ libpebble3/src/commonMain/kotlin/io/rebble/libpebblecommon/services/appmessage/AppMessageService.kt
@@ -9,12 +9,10 @@ import io.rebble.libpebblecommon.packets.AppCustomizationSetStockAppTitleMessage
 import io.rebble.libpebblecommon.packets.AppMessage
 import io.rebble.libpebblecommon.packets.AppMessageTuple
 import io.rebble.libpebblecommon.services.ProtocolService
-import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flatMapConcat
 import kotlinx.coroutines.flow.launchIn
@@ -22,7 +20,6 @@ import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
-import kotlinx.coroutines.withTimeout
 import kotlinx.coroutines.withTimeoutOrNull
 import kotlin.time.Duration.Companion.seconds
 import kotlin.uuid.Uuid
@@ -66,7 +63,7 @@ class AppMessageService(
                 }
             } ?: run {
                 logger.w { "Timed out sending AppMessage ${appMessageData.transactionId}" }
-                AppMessageResult.NACK(appMessageData.transactionId)
+                AppMessage.AppMessageNACK(appMessageData.transactionId)
             }
         }
         protocolHandler.send(appMessage)
